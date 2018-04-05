5138ab52054845c21d520ec4ea7b3281a7829adc
olegs-codefresh <olegs@codefresh.io>
2018-04-05 18:13:21 +0300

    update welcome message

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from Kubernetes TLV $(CF_COMMIT_URL)</h1></body></html' > /www/index.html
+echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from Kubernetes TLV</h1></body></html' > /www/index.html
 
 python -m SimpleHTTPServer 80

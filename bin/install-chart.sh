1856345667a0b000b4379b06e6f5d2f8e52365a0
olegs-codefresh <olegs@codefresh.io>
2018-04-05 15:43:32 +0300

    wip

--- bin/install-chart.sh
+++ bin/install-chart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 runCodefreshCmd(){
-    cmd = "codefresh install-chart"
-    cmd = "$cmd --cluster $WORKING_CLUSTER"
-    cmd = "$cmd --namespace $NAMESPACE"
-    cmd = "$cmd --repository $HELM_REPO_NAME"
+    cmd="codefresh install-chart"
+    cmd="$cmd --cluster $WORKING_CLUSTER"
+    cmd="$cmd --namespace $NAMESPACE"
+    cmd="$cmd --repository $HELM_REPO_NAME"
 
     echo "Running install cmd: $cmd"
     eval cmd

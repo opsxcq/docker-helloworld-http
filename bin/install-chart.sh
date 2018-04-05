9c350fa8e93f93a08926e09740cd12efdf314381
olegs-codefresh <olegs@codefresh.io>
2018-04-05 18:08:10 +0300

    include in release name the branch

--- bin/install-chart.sh
+++ bin/install-chart.sh
@@ -6,7 +6,8 @@ runCodefreshCmd(){
     cmd="$cmd --cluster $WORKING_CLUSTER"
     cmd="$cmd --namespace $NAMESPACE"
     cmd="$cmd --repository $HELM_REPO_NAME"
-    cmd="$cmd --name $CF_BRANCH_TAG_NORMALIZED"
+    cmd="$cmd --name $CHART_NAME"
+    cmd="$cmd --release-name $CF_BRANCH_TAG_NORMALIZED"
     cmd="$cmd --version $VERSION"
 
     echo "Running install cmd: $cmd"

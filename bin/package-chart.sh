591b88772b37003f53a27ccbd2cc03efd16bb3f4
olegs-codefresh <olegs@codefresh.io>
2018-04-05 11:20:21 +0300

    package chart

--- bin/package-chart.sh
+++ bin/package-chart.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# repository path in codefresh volume
+repo_dir=$CF_VOLUME_PATH/docker-helloworld-http
+
+# chart director
+chart_dir=$repo_dir/docker-helloworld-http
+
+# chart version based on version from VERSION file and current branch tag
+chart_version=$(echo $(cat $repo_dir/VERSION)-$CF_BRANCH_TAG_NORMALIZED)
+
 updateValuesWithCurrentImageTag(){
     yq '.imageTag = env.CF_BRANCH_TAG_NORMALIZED' $chart_dir/values.yaml --yaml-output > $CF_VOLUME_PATH/values.new.yaml
     mv $CF_VOLUME_PATH/values.new.yaml $chart_dir/values.yaml
@@ -11,17 +20,9 @@ updateChartSourceWithCommitUrl(){
 }
 
 packageChart(){
-    chrt_path=$(helm package $chart_dir --version $CHRAT_FULL_SEMVER --destination $CF_VOLUME_PATH | cut -d " " -f 8 )
+    chrt_path=$(helm package $chart_dir --version $chart_version --destination $CF_VOLUME_PATH | cut -d " " -f 8 )
 }
 
-# repository path in codefresh volume
-repo_dir=$CF_VOLUME_PATH/docker-helloworld-http
-
-# chart director
-chart_dir=$repo_dir/docker-helloworld-http
-
-# chart version based on version from VERSION file and current branch tag
-chart_version=$(echo $(cat $repo_dir/VERSION)-$CF_BRANCH_TAG_NORMALIZED)
 echo $chart_version
 
 $(updateValuesWithCurrentImageTag)
@@ -31,4 +32,5 @@ $(updateChartSourceWithCommitUrl)
 cat $chart_dir/Chart.yaml
 
 $(packageChart)
+ls $repo_dir
 

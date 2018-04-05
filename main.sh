9ea6e5b90fef6f6d854ac194dc829b28038b7a56
olegs-codefresh <olegs@codefresh.io>
2018-04-05 15:17:11 +0300

    Print commit message

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from '$(hostname)'</h1></body></html' > /www/index.html
+echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from '$(CF_COMMIT_MESSAGE)'</h1></body></html' > /www/index.html
 
 python -m SimpleHTTPServer 80
